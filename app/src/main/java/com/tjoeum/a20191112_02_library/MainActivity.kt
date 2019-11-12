ff246bee676f16c3a682a5cb520bc728f5e27dbd
Minsung89 <minsung0217@gmail.com>
2019-11-12 22:04:26 +0900

    글라이드로 이미지 넣기 예제

--- app/src/main/java/com/tjoeum/a20191112_02_library/MainActivity.kt
+++ app/src/main/java/com/tjoeum/a20191112_02_library/MainActivity.kt
@@ -2,16 +2,23 @@ package com.tjoeum.a20191112_02_library
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setValues()
+        setupEvents()
     }
 
 
     override fun setValues() {
+
+
+        Glide.with(this).load("http://image.kyobobook.co.kr/newimages/giftshop_new/goods/400/1539/S1458632370750.jpg").into(imgView)
     }
 
     override fun setupEvents() {
